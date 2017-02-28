3ee4cf38452a0c78a1085435c22307e64c130639
connorwalsh <c@polygon.pizza>
2017-02-28 11:52:53 -0500

    [cw] updated magit graph color custom option

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(magit-log-arguments (quote ("--graph" "--color" "--decorate" "-n256")))
  '(package-selected-packages
    (quote
     (matrix-client zop-to-char zenburn-theme yaml-mode which-key web-mode volatile-highlights vkill use-package undo-tree transpose-frame tern-auto-complete smartrep smartparens smart-mode-line seti-theme rspec-mode robe react-snippets rainbow-delimiters projectile-rails paredit ov operate-on-number notmuch nodejs-repl multiple-cursors multi-term move-text markdown-mode magit-gh-pulls latex-preview-pane jsx-mode json-mode js-comint imenu-anywhere iedit ido-ubiquitous helm-projectile helm-ag guru-mode grizzl golint god-mode go-rename go-playground go-eldoc go-autocomplete gitignore-mode gitconfig-mode git-timemachine gist flycheck fixme-mode expand-region exec-path-from-shell elpy ein easy-kill dracula-theme dockerfile-mode discover-my-major diff-hl crux company-tern color-theme-wombat color-identifiers-mode clojure-mode-extra-font-locking cider browse-kill-ring beacon auctex anzu ample-theme aggressive-indent ace-window ac-js2))))
